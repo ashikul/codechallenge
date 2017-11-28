be701289f437ebb16f257835299b950ccc61c9a8
Ashikul Alam <alam.ashikul@gmail.com>
2017-11-27 20:59:57 -0500

    update question 3

--- question3.js
+++ question3.js
@@ -2,6 +2,31 @@ $(document).ready(function () {
 
 	//TODO: incomplete
 
+    // var init = "RR..LRL";
+    // var animation = new Animation(init);
+    // Displays "XX..XXX"
+    // animation.animate(3);
+    // Displays ".X.XX.."
 
+ 
 
 });
+
+class Animation {
+
+    constructor(init) {
+        this.init = init;
+        //because particles can overlap, a string data structure isn't enough
+        //I would try an array of arrays
+    }
+
+    animate(speed) {
+        //loop through string
+        //move char L negative array position
+        //move char R positive array position
+    }
+
+    output() {
+        //output init, where L and R are represented as X
+    }
+}
